f539d69286c293034aa1ad79948f6fca8459ba28
Victor <victor.quertelet@gmail.com>
2025-01-29 16:56:42 +0100

    Improved code and added random id generation and DELETE method

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser')
+const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 const PORT = 3000;
@@ -8,34 +8,53 @@ const PORT = 3000;
 const cors = require('cors');
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/employees', (req, res) => {
-    fs.readFile('employees.json', 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error rading employee data')
-        }
-        res.send(JSON.parse(data))
-    })
+app.get('/employees', async (req, res) => {
+    try {
+        const data = await fs.readFile('employees.json', 'utf8')
+        res.json(JSON.parse(data))
+    } catch (err) {
+        res.status(500).send('Error reading employee data')
+    }
+
 })
 
-app.post('/employees', (req, res) => {
-    const newEmployee = req.body;
-
-    fs.readFile('employees.json', 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading employee data')
-        }
-
+app.post('/employees', async (req, res) => {
+    try {
+        const data = await fs.readFile('employees.json', 'utf8')
         const employees = JSON.parse(data);
+
+        const newEmployee = {
+            id: uuidv4(),
+            ...req.body,
+            createdAt: new Date().toISOString()
+        };
+
         employees.push(newEmployee)
-        fs.writeFile('employees.json', JSON.stringify(employees, null, 2), (writeErr) => {
-            if (writeErr) {
-                return res.status(500).send('Error saving employee data')
-            }
-            res.status(201).send(newEmployee)
-        })
-    })
+
+        await fs.writeFile('employees.json', JSON.stringify(employees, null, 2))
+
+        res.status(201).json(newEmployee)
+    } catch (err) {
+        res.status(500).send('Error creating employee')
+    }
+
+})
+
+app.delete('/employees/:id', async (req, res) => {
+    try {
+        const data = await fs.readFile('employees.json', 'utf-8')
+        let employees = JSON.parse(data)
+
+        const employeeId = req.params.id
+        employees = employees.filter(emp => emp.id !== employeeId)
+
+        await fs.writeFile('employees.json', JSON.stringify(employees, null, 2))
+        res.status(200).send('Employee successfully removed')
+    } catch (err) {
+        res.status(500).send('Error deleting employee')
+    }
 })
 
 app.listen(PORT, () => {
